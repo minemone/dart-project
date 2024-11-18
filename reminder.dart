006931176afa5b6ccaeb5cc3d374e590756545d0
unknown <mone23170@gmail.com>
2024-11-18 15:06:09 +0700

    first commit

---
+++ reminder.dart
@@ -0,0 +1,12 @@
+void main(){
+  int num1 = 10;
+  int num2 = 3;
+
+  int quotient  = num1~/num2;
+  int remainder = num1%num2;
+
+  print("output");
+  print("quotient = $quotient");
+  print("remainder = $remainder");
+  
+}
