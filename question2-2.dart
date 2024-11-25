b8c7ecee03eb98b5cef6cfe9cdb6a27653c5011c
unknown <mone23170@gmail.com>
2024-11-25 14:43:36 +0700

    question 2

---
+++ question2-2.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+void main() {
+
+  stdout.write('Enter a character: ');
+  var char = stdin.readLineSync();
+  var lowerChar = char?.toLowerCase();
+
+  switch(lowerChar) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+      print("$char is a vowel");
+      break;
+    default:
+      print("$char is a consonant");
+  }
+
+ 
+  
+}
